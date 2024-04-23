a0203421f590224ea85820ab3dc44440aa386bc3
Jakub Antonín Štigler <jakubstig@gmail.com>
2024-04-23 13:01:24 +0200

    Add install command

---
+++ useful_stuff/makemakeup.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/sh
+
+# install script for makemake
+# wget -O - | sh && sudo cp /tmp/makemake-rs-/target/release/makemake /usr/bin/makemake && cp "/tmp/makemake/useful_stuff/man-page/makemake.7" "/usr/share/man/man7/makemake.7"
+
+check_exists() {
+    if type $1 1>/dev/null 2> /dev/null; then
+        true
+    else
+        echo "Missing the program '$1'."
+        exit 1
+    fi
+}
+
+check_exists wget
+check_exists cargo
+
+VERSION=2.1.0
+PKG="https://github.com/BonnyAD9/makemake-rs/archive/refs/tags/v$VERSION.tar.gz"
+
+cd /tmp
+
+if wget -O - "$PKG" | tar xz; then
+    true
+else
+    exit 1
+fi
+
+mv "makemake-rs-$VERSION" makemake
+cd makemake
+
+if cargo build -r; then
+    true
+else
+    exit 1
+fi
